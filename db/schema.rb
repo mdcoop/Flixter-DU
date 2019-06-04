525ef885feb040fe2ec44f704e34446d6ced47d3
Madison Cooper <madisondcooper@gmail.com>
2019-06-04 02:21:28 +0000

    Video Uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_232129) do
+ActiveRecord::Schema.define(version: 2019_06_04_020741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_232129) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
